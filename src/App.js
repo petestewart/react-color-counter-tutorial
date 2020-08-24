6737cbac5dcf0cadc77a1dbb215d03d938eced67
Pete Stewart <petestew@gmail.com>
2020-08-24 09:26:17 -0500

    tutorial complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import randomcolor from 'randomcolor';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState('');
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    setColor(randomcolor());
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ color: color }}>{count}</h1>
+      <button onClick={increaseCount}>Change!</button>
     </div>
   );
-}
+};
 
 export default App;
